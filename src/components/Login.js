a3b06747c9612d6860b5cb45781ff1856d7d3e28
lirenmi <lirenmi@163.com>
2019-10-16 11:17:58 +0800

    导入CSS

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,23 +3,25 @@ import React from 'react';
 class Login extends React.Component {
   render() {
     return (
-      <form className="login-wrapper">
-        <div>
-          <label>Email</label>
-          <div>
-            <input type="text" placeholder="Email" />
+      <div className="login-wrapper">
+        <form className="box login-box">
+          <div className="field">
+            <label className="label">Email</label>
+            <div className="control">
+              <input className="input" type="text" placeholder="Email" />
+            </div>
           </div>
-        </div>
-        <div>
-          <label>Password</label>
-          <div>
-            <input type="text" placeholder="Password" />
+          <div className="field">
+            <label className="label">Password</label>
+            <div className="control">
+              <input className="input" type="password" placeholder="Password" />
+            </div>
           </div>
-        </div>
-        <div>
-          <button>Login</button>
-        </div>
-      </form>
+          <div className="control">
+            <button className="button is-fullwidth is-primary">Login</button>
+          </div>
+        </form>
+      </div>
     );
     // JSX  Babel  Emmet
   }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Login from './components/Login';
+import './css/app.scss';
+import './css/style.scss';
 
 ReactDOM.render(<Login />, document.getElementById('root'));
+
+// https://github.com/lirenmi/react-store-start
