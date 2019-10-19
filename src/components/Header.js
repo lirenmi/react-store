9f2f50728488d623c4eb52e4c20423f0a95bc595
lirenmi <lirenmi@163.com>
2019-10-19 15:59:45 +0800

    函数式组件

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,26 @@
 import React from 'react';
 
-class Header extends React.Component {
-  renderLink() {
-    const nickname = this.props.nickname;
-    if (nickname) {
-      return (
-        <span className="nickname">
-          <i className="far fa-user"></i>
-          {this.props.nickname}
-        </span>
-      );
-    } else {
-      return (
-        <React.Fragment>
-          <a href="/">Login</a>
-          <a href="/">Register</a>
-        </React.Fragment>
-      );
-    }
-  }
-  render() {
-    return (
-      <div className="header">
-        <div className="grid">
-          <div className="start">
-            <a href="/">Home</a>
-          </div>
-          <div className="end">{this.renderLink()}</div>
-        </div>
+const Header = props => (
+  <div className="header">
+    <div className="grid">
+      <div className="start">
+        <a href="/">Home</a>
       </div>
-    );
-  }
-}
+      <div className="end">
+        {props.nickname ? (
+          <span className="nickname">
+            <i className="far fa-user"></i>
+            {props.nickname}
+          </span>
+        ) : (
+          <React.Fragment>
+            <a href="/">Login</a>
+            <a href="/">Register</a>
+          </React.Fragment>
+        )}
+      </div>
+    </div>
+  </div>
+);
 
 export default Header;
