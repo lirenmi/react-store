36f7de30de1bab2e4ac3c8496308ebc15e4ce791
lirenmi <lirenmi@163.com>
2019-10-20 17:42:44 +0800

    Refs访问元素

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,23 +1,25 @@
 import React from 'react';
 
 class Login extends React.Component {
-  /**
-    1. 命名和绑定
-    2. event
-    3. this
-    4. 传递参数
-   */
+  // Refs
+  emailRef = React.createRef();
+  passwordRef = React.createRef();
 
   handleSubmit = event => {
     // 1. 阻止默认事件行为
     event.preventDefault();
 
     // 2. 获取表单数据
+    const formData = {
+      email: this.emailRef.current.value,
+      password: this.passwordRef.current.value
+    };
+    console.log(formData);
 
     // 3. 处理登录逻辑
 
     // 4. 跳转到首页视图
-    this.props.history.push('/');
+    // this.props.history.push('/');
   };
 
   render() {
@@ -27,13 +29,23 @@ class Login extends React.Component {
           <div className="field">
             <label className="label">Email</label>
             <div className="control">
-              <input className="input" type="text" placeholder="Email" />
+              <input
+                className="input"
+                type="text"
+                placeholder="Email"
+                ref={this.emailRef}
+              />
             </div>
           </div>
           <div className="field">
             <label className="label">Password</label>
             <div className="control">
-              <input className="input" type="password" placeholder="Password" />
+              <input
+                className="input"
+                type="password"
+                placeholder="Password"
+                ref={this.passwordRef}
+              />
             </div>
           </div>
           <div className="control">
