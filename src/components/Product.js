859ca8e018af7d7f87ccfe88ceb8c30e5ddd9180
lirenmi <lirenmi@163.com>
2019-10-21 15:56:56 +0800

    商品组件布局

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,7 +4,22 @@ class Product extends React.Component {
   render() {
     return (
       <div className="product">
-        <p>Product</p>
+        <div className="p-content">
+          <div className="img-wrapper">
+            <figure className="image is-4by3">
+              <img src="images/2.jpg" alt="Nike Paul George PG 3" />
+            </figure>
+            <p className="p-tags">25 Colors</p>
+            <p className="p-name">Nike Paul George PG 3</p>
+          </div>
+        </div>
+        <div className="p-footer">
+          <p className="price">￥538.00</p>
+          <button className="add-cart">
+            <i className="fas fa-shopping-cart"></i>
+            <i className="fas fa-exclamation"></i>
+          </button>
+        </div>
       </div>
     );
   }
