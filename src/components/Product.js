d0d2f253cf36a0586957210238413f799e78df19
lirenmi <lirenmi@163.com>
2019-10-22 11:26:26 +0800

    商品库存UI逻辑处理

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,21 +3,26 @@ import { formatPrice } from 'commons/helper';
 
 class Product extends React.Component {
   render() {
-    const { name, image, tags, price } = this.props.product;
+    const { name, image, tags, price, status } = this.props.product;
+    const _pClass = {
+      available: 'product',
+      unavailable: 'product out-stock'
+    };
     return (
-      <div className="product">
+      <div className={_pClass[status]}>
         <div className="p-content">
           <div className="img-wrapper">
+            <div className="out-stock-text">Out Of Stock</div>
             <figure className="image is-4by3">
               <img src={image} alt={name} />
             </figure>
-            <p className="p-tags">{tags}</p>
-            <p className="p-name">{name}</p>
           </div>
+          <p className="p-tags">{tags}</p>
+          <p className="p-name">{name}</p>
         </div>
         <div className="p-footer">
           <p className="price">{formatPrice(price)}</p>
-          <button className="add-cart">
+          <button className="add-cart" disabled={status === 'unavailable'}>
             <i className="fas fa-shopping-cart"></i>
             <i className="fas fa-exclamation"></i>
           </button>
