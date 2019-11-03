cd9e2fd30ca1268ad6704e5a571af2caddfbc3a5
lirenmi <lirenmi@163.com>
2019-11-03 22:56:35 +0800

    项目部署

--- src/commons/axios.js
+++ src/commons/axios.js
@@ -2,7 +2,8 @@ import _axios from 'axios';
 
 const axios = baseURL => {
   const instance = _axios.create({
-    baseURL: baseURL || 'http://localhost:3003',
+    baseURL:
+      baseURL || process.env.REACT_APP_API_DOMAIN || 'http://localhost:3003',
     timeout: 1000
   });
 
